d2181cd06447b644b57e3b26da882c462a25429c
Kikoooohub <krastev182@alumnos.cei.es>
2024-12-17 15:07:59 +0100

    dia38

--- dia37/js/script.js
+++ dia37/js/script.js
@@ -7,13 +7,57 @@
 
 
 const sliderImages = document.querySelector('.Slider-images');
-const images = document.querySelectorAll('.Slider-img');
+//const images = document.querySelectorAll('.Slider-img');
+const images = []; // lista de elementos HTML con imagenes dentro.
 const btnNext = document.querySelector('#btnNext');
 const btnPrev = document.querySelector('#btnPrev');
 const spanActual = document.getElementById('actual');
-const spanTotal = document.getElementById('total');
+let spanTotal = document.getElementById('total');
 let currentImageIndex = 0;
-const totalImages = images.length;
+const totalImages = images.length; // devuelve 0,porque images encotre imagenes en el HTML
+
+
+
+// lista de imagenes
+const listaImagenes = [
+    {
+        file: "imagen1.jpg", description: "imagen 1"
+    },
+
+    { file: "imagen2.jpg", description: "imagen 2" },
+    { file: "imagen3.jpg", description: "imagen 3" },
+    { file: "imagen4-long.jpg", description: "imagen 4" },
+    { file: "imagen1.jpg", description: "imagen 1" },
+    { file: "imagen2.jpg", description: "imagen 2" },
+    { file: "imagen3.jpg", description: "imagen 3" }
+];
+
+
+// Metodo 2: Lista de imagenes en JSON (string)
+/*const listaImagenes_json = `[
+{file: "imagen1.jpg", description: "imagen 1"},
+
+    { file: "imagen2.jpg", description: "imagen 2" },
+    { file: "imagen3.jpg", description: "imagen 3" },
+    { file: "imagen4-long.jpg", description: "imagen4"},
+    { file: "imagen1.jpg", description: "imagen 1" },
+    { file: "imagen2.jpg", description: "imagen 2" },
+    { file: "imagen3.jpg", description: "imagen 3" }
+]`;
+
+// con JSON.parse (miStringDeJSON) convertimos de JSON A Objeto JS.
+const listaImagenes = JSON.parse(listaImagenes_json);
+
+// Si quisiera convertir de JS a JSON usaria:
+const stringJson = JSON.stringify(listaImagenes);
+console.log(stringJson);*/
+
+//metodo 3: Lista de imagenes obtenida desde una "API" con promesas/fetch
+
+function traerImagenesAPI(){
+    fetch//('https://jsonplaceholder.typicode.com/photos')
+    fetch('./basededatos.json').then(response => response.json());// esto es un JSON parse
+}
 
 
 
@@ -47,11 +91,63 @@ function actualizarSlider() {
 function actualizarContador() {
     spanActual.textContent = currentImageIndex + 1;
 }
- //---------------------------------------------------
+function cargarImagenes() {
+
+
+    listaImagenes.map((imagen) => {
+        console.log("Cargando imagenes del objeto js listaImagenes");
+
+        //metodo inseguro y seceptible a inyecion de codigo
+        /* sliderImages.innerHTML +=
+                `<img src="0" oneerror="alert('tengo acceso a t navegador')"/>`
+ 
+ 
+            `<img class="Slider-img" src="img/${imagen.file}" alt="${imagen.description}">`;*/
+
+
+
+
+        //METODO SEGURO Y DE MAYOR CONTROL!
+
+
+
+        const img = document.createElement('img');// crear etiqueta <img>
+        img.classList.add('Slider-img');//agregar clase "Slider-img"
+        img.scr = 'img/${imagen.file}';// establecer el src de la imagen
+        img.alt = imagen.description;// establecer el alt de la imagen
+
+
+        //img.textContent ="el texto que quiera dentro de la etiqueta";
+        //img.setAttribute('edad',38);   //<img edad=38 />
+        //img.addEventListener("clcik en la imagen"); //
+        //console.log("click en imagen");
+    //})
+
+        sliderImages.appendChild(img); // agregar la imagen al sliderImages
+
+
+
+        //images = document.querySelectorAll('.Slider-img');
+        images.push(img);    
+
+
+
+    });
+
+    // ahora que tenemos el HTML cargado, contamos la cantidad de imagenes.
+
+    totalImages = images.length;
+    spanTotal.textContent = totalImages;
+    actualizarContador();
+
+};
+
+
+//---------------------------------------------------
 // 3. Inicializo los contadores/Ejecutamos nuestro codigo
 //---------------------------------------------------
-spanTotal.textContent = totalImages;
-actualizarContador();
+cargarImagenes();
+
 
 
 //---------------------------------------------------
@@ -68,4 +164,6 @@ sliderImages.addEventListener('mouseout', () => {
 function agregarIntervalo() {
     intervalos = setInterval(nextImage, 1000);
 }
+
+
 
---
+++ dia37/readme.md
@@ -0,0 +1,48 @@
+## Creacion de eitquetas HTML con JS
+
+El uso innerHTML es rapido y sencillo.Es util para grandes bloques de contenido.
+
+
+```js
+
+let div = document.getElementById("#container");
+div.innerHTML = `
+<style> body { background-color:  red; }</style>
+<h1>titulo</h1>
+`;
+```
+
+
+Alternativamente podemos crear elemntos HTML de forma programcatica
+Desventajas:
+
+-Requiere mas pasos para construir el HTML
+Ventajas:
+
+-Ofrece control total sobre los elemntos
+-Es mas seguro (ya que podemos usar textContent en vez de innerHTML).
+
+
+```JS
+
+let  fragmento = document.createDocumentFragment();// etiqueta invisble "<>"
+
+let div = document.createElement("div"); // creamos un div
+
+div.classList.add("clase1","clase2"); //agregar una o msa clases  <div class= `clase1 clase2`> </div>
+div.id = "idCaja";
+
+div.textContent = "<h1>Caja</h1>"; // lso h1 se muestro como texto
+div.setAttribute ("data-color", "red"); // <div id="idCaja" class=" class='clase1 clase 2' data-color='red'>  <h1>'titulo'</h1> </div>
+div.addEventListner ("click",() => { console.log("click en el div")});
+fragment.appendChild(div); // lo metemos en el fragmento
+
+
+document.body.appendChild(fragmento);  //  agrega nuestro  al final del body
+// o a este otro (pero no ambos)
+
+
+document.body.insertBefore(fragmento,docuemnt.body.firstChild); // al principio del body.
+```
+
+
